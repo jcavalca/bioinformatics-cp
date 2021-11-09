84074587bff05b8ad2d9b8c03495ab2f8a93d764
Ping-Wei Lin <ping-wei@Ping-Weis-MacBook-Pro.local>
2021-11-09 15:20:01 -0800

    starter code for prompting GEMMA model

--- run_vcf2gwas.py
+++ run_vcf2gwas.py
@@ -14,7 +14,33 @@ def main():
         print("One or more files cannot be found")
         return
 
+    GEMMA_Options()
 
+    return 1
+
+def GEMMA_Options():
+    print("Here are the questions regarded about GEMMA models:")
+    print("What model do you want to run?")
+    print("     (1) Linear Model Analysis")
+    print("     (2) Linear Mixed Model Analysis")
+    print("     (3) Bayesian Sparse Linear Mixed Model Analysis")
+    print("Please enter 1, 2, or 3: ", end=" ")
+    model_type = input()
+
+    if (model_type == "1"):
+        command = "-p 1 -lm"
+    elif (model_type == "2"):
+        # still need to fix what if only one phenotype is given
+        print("What phenotypes do you want to run?")
+        print("For example, if you want to run CD8 (1st column), MCH (2nd column), and avrRpm (4th column) phenotype independently," +
+            "you can enter CD8 MCH avrRpm, or you can enter 1 2 4")
+        print("Please enter name of phenotype(s) listed on your phenotype file or coloumn(s) of the phenotype(s): ", end=" ")
+        number_of_phenotypes = input().split(" ")
+        number_of_phenotypes = " -p ".join(number_of_phenotypes)
+        command = f"-p {number_of_phenotypes} -lmm --multi"
+        print(command)
+    # elif (model_type == "3"):
+        
     return 1
 
 if __name__ == "__main__":
